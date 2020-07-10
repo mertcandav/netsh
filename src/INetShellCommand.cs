55eec8b9711b1979bf8fbebf62a4c84fe51fbc5e
Mertcan Davulcu <mertcand450@hotmail.com>
2020-07-10 14:13:12 +0300

    Add INetShellCommand.

---
+++ src/INetShellCommand.cs
@@ -0,0 +1,20 @@
+﻿namespace NetSh {
+    /// <summary>
+    /// Interface for NetShell commands.
+    /// </summary>
+    public interface INetShellCommand {
+        #region Properties
+
+        /// <summary>
+        /// Command.
+        /// </summary>
+        string Command { get; set; }
+
+        /// <summary>
+        /// Description of command.
+        /// </summary>
+        string Description { get; set; }
+
+        #endregion
+    }
+}

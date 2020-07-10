c6786690287e6b5dd0286720bf250d5e6f5096d2
Mertcan Davulcu <mertcand450@hotmail.com>
2020-07-10 14:41:02 +0300

    Add NetShellCommand.

--- fstest/Program.fs
+++ fstest/Program.fs
@@ -1,7 +1,12 @@
 ﻿namespace fstest
 
+open System
+open NetSh
+
 module main =
     [<EntryPoint>]
     let main argv =
         printfn "F# test application"
+        let mutable command = new NetShellCommand(String.Empty,String.Empty)
+        command.Command <- "Exit"
         0
---
+++ src/NetShellCommand.cs
@@ -0,0 +1,43 @@
+﻿namespace NetSh {
+    /// <summary>
+    /// Command for NetShell.
+    /// </summary>
+    public struct NetShellCommand:INetShellCommand {
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cmd">Command.</param>
+        public NetShellCommand(string cmd) {
+            Command = cmd;
+            Description = string.Empty;
+        }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="cmd">Command.</param>
+        /// <param name="desc">Description.</param>
+        public NetShellCommand(string cmd,string desc) :
+            this(cmd) {
+            Description = desc;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Command.
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// Description of command.
+        /// </summary>
+        public string Description { get; set; }
+
+        #endregion
+    }
+}

cb1238201675f70f9e3974c7eaab9e1df7921464
Mertcan Davulcu <mertcand450@hotmail.com>
2020-07-11 10:40:20 +0300

    Add extension functions for Char.

---
+++ src/Extensions/Char.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NetSh.Extensions {
+    /// <summary>
+    /// Extension functions for <see cref="System.Char"/> samples.
+    /// </summary>
+    public static class Char {
+        /// <summary>
+        /// Write.
+        /// </summary>
+        /// <param name="ch">Char to write.</param>
+        public static void Write(this char ch) {
+            Console.Write(ch);
+        }
+
+        /// <summary>
+        /// WriteLine.
+        /// </summary>
+        /// <param name="ch">Char to write.</param>
+        public static void WriteLine(this char ch) {
+            Console.Write(ch);
+        }
+    }
+}

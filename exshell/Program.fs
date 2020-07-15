f8e4eacf41cd91d9dcad137e6fa5d7329296d777
Mertcan Davulcu <mertcand450@hotmail.com>
2020-07-15 19:37:45 +0300

    Add ls command to exshell.

--- exshell/Program.fs
+++ exshell/Program.fs
@@ -1,6 +1,7 @@
 ﻿open System;
 open System.Collections;
 open System.Collections.Generic;
+open System.IO;
 open NetSh;
 open NetSh.Tools;
 open NetSh.Extensions;
@@ -17,7 +18,7 @@ let main(argv) =
     shell.Mode <- NetShellMode.Namespace;
     shell.Prompt <- __SOURCE_DIRECTORY__;
     shell.AfterPrompt.Add(AfterPrompt);
-    shell.AddCmd("cd","Manage current path.",fun (command:INetShellCommand) (input:string) ->
+    shell.AddCmd("cd","Manage current path.",fun (command: INetShellCommand) (input: string) ->
         let mutable cmd = input.RemoveNamespace().Trim();
         if cmd = ".." then
             shell.Prompt <- path.previous(shell.Prompt);
@@ -25,6 +26,20 @@ let main(argv) =
             "Command is not defined!".Println(ConsoleColor.Red);
         else
             shell.Prompt <- path.join(shell.Prompt,cmd);
+        );
+    shell.AddCmd("ls","List this directory.",fun (command: INetShellCommand) (input: string) ->
+        let mutable cmd = input.RemoveNamespace();
+        if input.ToLower() = "ls" then
+            cmd <- "ls -f -d";
+        let mutable parameters = cmd.GetParameters("-");
+        for cparam in parameters do
+            let mutable param = cparam.Trim().ToLower();
+            if param = "-f" then
+                Directory.GetFiles(shell.Prompt).WriteLine();
+            else if param = "-d" then
+                Directory.GetDirectories(shell.Prompt).WriteLine();
+            else
+                printfn "'%s' is not defined!" cparam;
         );
     shell.Loop();
 

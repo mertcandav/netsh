2ef66c7a79cd62251e735553962c0b82b26b4a7c
Mertcan Davulcu <mertcand450@hotmail.com>
2020-07-15 18:50:33 +0300

    Add exshell.

---
+++ exshell/Program.fs
@@ -0,0 +1,21 @@
+﻿open System;
+open System.Collections;
+open System.Collections.Generic;
+open NetSh;
+open NetSh.Tools;
+open NetSh.Extensions;
+open SNetShell;
+
+let mutable shell = new SNetShell();
+
+let AfterPrompt(e: EventArgs) =
+    "$ ".Print(ConsoleColor.DarkYellow);
+
+[<EntryPoint>]
+let main(argv) =
+    printfn "Example Shell of NetShell";
+    shell.Prompt <- __SOURCE_DIRECTORY__;
+    shell.AfterPrompt.Add(AfterPrompt);
+    shell.Loop();
+
+    0;
---
+++ exshell/SNetShell.fs
@@ -0,0 +1,13 @@
+﻿
+module SNetShell
+
+open System;
+open NetSh;
+
+type SNetShell() =
+    inherit NetShell()
+
+    override this.Help(spacecount: int) =
+        printfn "";
+        base.Help(spacecount);
+        printfn "";
